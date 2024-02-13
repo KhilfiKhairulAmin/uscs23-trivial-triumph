b6458d3cbe9f2dafc89396782e877f174eb04a14
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-02-13 22:41:24 +0800

    Update Yasmin parts

--- quiz.py
+++ quiz.py
@@ -1,6 +1,26 @@
 import random
 
-## MY CHANGES STARTS HERE \/
+questionsMatch=[("What is the scientific term for the “little brain” at the base of the brain that coordinates movement and balance?", "cerebellum"),
+                ("What is the chemical symbol for the element mercury?", "hg"),
+                ("What is the official animal of Scotland?", "unicorn"),
+                ("What animal is known to laugh and has been proven to have a sense of humor?", "rats"),
+                ("What animal’s milk is pink?", "hippopotamus")
+]
+
+questionsFIB=[("Q", "Modern", "family"),
+              ("Q", "Gilmore", "girls"),
+              ("Q", "The Big Bang", "theory"),
+              ("Q", "Mission:", "impossible")
+              ("Q", "Grey's", "anatomy")
+]
+
+questionsSub=[("What is the only food that can never go bad?", "honey"),
+              ("Who is Barbie’s little sister?", "skipper"),
+              ("Anna, Elsa Kristoff and Olaf are all characters in what animated movie?", "frozen"),
+              ("Which popular condiment was once sold as a medicinal cure for diarrhea?", "ketchup"),
+              ("Which country do cities of Perth, Adelade & Brisbane belong to?", "australia"),
+]
+
 
 def load_mcq_questions():
     f = open("mcq.txt")
@@ -39,21 +59,43 @@ def load_tf_questions():
     return tf
 
 
-questionsMCQ = load_mcq_questions()
+def load_FIB_questions():
+    f = open("FIB.txt")
 
+    for _ in range (2):
+        next(f)
+
+    fib = []
+    for _ in f:
+        question = next(f).strip()
+        correct_answer =                    #??????
+        FIB_append((question, correct_answer))
+
+
+questionsMCQ = load_mcq_questions()
 questionsTF = load_tf_questions()
 
-## MY CHANGES ENDS HERE  ^
 
-def quiz_Set1(questionsMCQ, questionsTF):
+
+def quizEasy_Set1(questionsMCQ, questionsTF, questionMatch, questionsFIB, questionsSub):
     score=0
     score+=Set1_MCQ(questionsMCQ, score)
     score+=Set1_TF(questionsTF, score)
+    score+=Set1_Match(questionsMatch, score)
+
+    if score>9:
+        score+=quizHard_FIB(questionsFIB, score)
+        score+=quizHard_Sub(questionsSub, score)
+    else:
+        pass
+    
     print("Quiz completed! Your score is:", score)
     
 def Set1_MCQ(questionsMCQ, score):
-    for count in range(1, 4, 1):
-        questionsNo = random.choices(range(len(questionsMCQ)), k=3)
+    print("\nMutiple Choice Questions\n")
+    
+    for count in range(1, 4):                    #only 3 questions are displayed
+        questionsNo=random.choices(range(len(questionsMCQ)), k=3)
         for index in questionsNo:
             question, options, answer = questionsMCQ[index]
             print(count, ".", question)
@@ -71,8 +113,9 @@ def Set1_MCQ(questionsMCQ, score):
 
 def Set1_TF(questionsTF, score):
     print("\nTRUE OR FALSE QUESTIONS\n")
+    
     for count in range(1, 4, 1):
-        questionsNo=random.sample(range(len(questionsTF)), len(questionsTF))
+        questionsNo=random.choices(range(len(questionsTF)), k=3)
         for index in questionsNo:
             question, answer = questionsTF[index]
             print(count, ".", question)
@@ -86,5 +129,64 @@ def Set1_TF(questionsTF, score):
                 break
     return score
 
-quiz_Set1(questionsMCQ, questionsTF)
+def Set1_Match(questionMatch, score):   #taktahu!!!
+    print("\nMatching Questions\n")
+    print("Match the statements <1,2,3> correctly to their answers <A,B,C>.")
+    
+    for count in range (1, 4, 1):
+        questionsNo=random.choices(range(len(questionsTF)), k=3)
+        for index in questionsNo:
+            question, answer = questionsMatch[index]
+            print(question, count, answer)
+            userAnswer=input("(",count,") : ").upper()
+            if userAnswer == answer:
+                score+=2
+                print("Correct!")
+                break
+            else:
+                print("Incorrect.")
+                break
+    return score
+
+
+def quizHard_FIB(questionsFIB, score):
+    print("\nHARD MODE\n")
+    print("Fill in the blanks.\n")
+
+    for count in range (1, 4):
+        questionsNo=random.choices(range(len(questionsFIB)), k=3)
+        for index in questionsNo:
+            question, answer = questionsFIB[index]
+            print(count, ".", question, "_" * len(answer))
+            userAnswer = input("Answer: ").lower()
+            if userAnswer == answer:
+                score+=5
+                print("Correct!!")
+                break
+            else:
+                print("Incorrect.")
+                break
+    return score
+
+def quizHard_Sub(questionsSub, score):
+    print("\nSubjective Questions\n")
+
+    for count in range (1, 4):
+        questionsNo = random.choices(range(len(questionsFIB)), k=3)
+        for index in questionsNo:
+            question, answer = questionsSub[index]
+            print(count, ".", question)
+            userAnswer = input("Answer: ").lower()
+            if userAnswer == answer:
+                score+=5
+                print("Correct!!")
+                break
+            else:
+                print("Incorrect.")
+                break
+    return score
+    
+            
+
+quizEasy_Set1(questionsMCQ, questionsTF, questionsMatch, questionsFIB, questionsSub)
 
