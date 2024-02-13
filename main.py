2f43099d51570b4e140500e43e373674ef1dc5a8
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-02-14 00:38:52 +0800

    Updated structure of mainpage

--- main.py
+++ main.py
@@ -6,32 +6,38 @@ from ui import *
 
 
 def main_page():
+  # Display header of main page
   display_header()
   center("Do you want to register or login?\n")
   center("[1] Register    [2] Login\n")
   
+  # Get choice from user
   while True:
-    choice = prompt("=>", input_width=2)
+    try:
+      choice = prompt(">", input_width=2)
 
-    if not choice.isdigit():
-      error("Please enter a number\n")
-      continue
+      # Choice must be integer
+      if not choice.isdigit():
+        raise ValueError("Please enter a number\n")
 
-    choice = int(choice)
+      # Choice must be between 1 and 2
+      if choice < 1 or choice > 2:
+        raise ValueError("Invalid choice\n")
 
-    if choice == 1:
-      sign_up_page()
-      break
-    elif choice == 2:
-      login_page()
-      break
+      choice = int(choice)
+      if choice == 1:
+        sign_up_page()  # Go to sign up
+      elif choice == 2:
+        login_page()  # Go to login
+
+    except ValueError as err:
+      error(err)
     else:
-      error("Invalid choice\n")
-      continue
+      break
 
 
 def login_page():
-  print(ASCII_ART)
+  print(TRIVIAL_TRIUMPH_ASCII_ART)
   center("Welcome to Trivial Triumph!\n")
   fill("*")
   print()
@@ -41,19 +47,30 @@ def login_page():
 
 
 def sign_up_page():
+  # Display header of sign up page
   display_header()
   center("Sign Up\n")
-  username = prompt("Username: ")
-  password = prompt("Password: ")
-  repeat_password = prompt("Repeat password: ")
-  auth.sign_up(username, password, repeat_password, users.keys())
-  pass
 
+  # Prompt user inputs
+  while True:
+    try:
+      username = prompt("Username: ")
+      password = prompt("Password: ")
+      repeat_password = prompt("Repeat password: ")
+      auth.sign_up(username, password, repeat_password, users.keys())
+    except ValueError as err:
+      error(err)
+    else:
+      break
+    
 
 def main():
   # Global variable containing all user data
   global users
   users = db.get_users_data()
+
+  # Start at main page
+  main_page()
 
 
 if __name__ == "__main__":
