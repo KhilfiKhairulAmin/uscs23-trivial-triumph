d1ee0c2fedcaada1339bbba3b3e3e1f4a6fb66b5
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-02-14 08:27:38 +0800

    Updated implementation of state machine Allows more clear abstractions

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ CurUser = ""
 # Global variable containing all user data.
 Users = db.get_users_data()
 
-# Global variable that store the current state of the application
-State = 0
-
 
 def main_page():
   """
@@ -24,8 +21,7 @@ def main_page():
   center("Do you want to register or login?\n")
   center("[1] Register    [2] Login    [3] Quit\n")
 
-  global State
-  while State == 0:
+  while True:
     try:
       choice = prompt(">", input_width=2)
 
@@ -37,6 +33,13 @@ def main_page():
       choice = int(choice)
       if choice < 1 or choice > 3:
         raise ValueError("Invalid choice")
+      
+      if choice == 1:
+        return 1  # Go to sign up
+      elif choice == 2:
+        return 2  # Go to login
+      elif choice == 3:
+        return -1  # Quit the app
 
     except ValueError as err:
       error(err)
@@ -44,14 +47,6 @@ def main_page():
     except KeyboardInterrupt:
       print()
       center("Please enter '3' to quit the application.\n")
-    else:
-      if choice == 1:
-        State = 1  # Go to sign up
-      elif choice == 2:
-        State = 2  # Go to login
-      elif choice == 3:
-        State = -1  # Quit the app
-
 
 
 def sign_up_page():
@@ -62,24 +57,24 @@ def sign_up_page():
   display_header()
   center("Sign Up\n")
 
-  global State
-  while State == 1:
+  while True:
     try:
       username = prompt("Username: ")
       password = prompt("Password: ", hidden=True)
       repeat_password = prompt("Repeat password: ", hidden=True)
       auth.sign_up(username, password, repeat_password, Users)  # Validate the input data for sign up
-    except ValueError as err:
-      error(err)
-    # If user press Ctrl+C, go back to main page
-    except KeyboardInterrupt:
-      State = 0
-    else:
+
       # Add the new user
       Users[username] = [password, -1]
       # Save the updated data
       db.save_users_data(Users)
-      State = 3
+    # Go to home page
+      return 3
+    except ValueError as err:
+      error(err)
+    # If user press Ctrl+C, go back to main page
+    except KeyboardInterrupt:
+      return 0  # Go back to main page
 
 
 def login_page():
@@ -90,22 +85,22 @@ def login_page():
   display_header()
   center("Log In\n")
 
-  global State
-  while State == 2:
+  while True:
     try:
       username = prompt("Username: ")
       password = prompt("Password: ", hidden=True)
       auth.log_in(username, password, Users)  # Validate the input data for sign up
-    except ValueError as err:
-      error(err)
-    # If user press Ctrl+C, go back to main page
-    except KeyboardInterrupt:
-      State = 0
-    else:
+
       # Update the global variables
       global CurUser
       CurUser = username
-      State = 3  # Go to home page
+      # Go to home page
+      return 3
+    except ValueError as err:
+      error(err)
+    except KeyboardInterrupt:
+      # If user press Ctrl+C, go back to main page
+      return 0
 
 
 def home_page():
@@ -115,8 +110,7 @@ def home_page():
   # Display header of home page
   display_header(subtitle=f"Welcome {CurUser}!")
 
-  global State
-  while State == 3:
+  while True:
     pass
     
 
@@ -124,17 +118,17 @@ def main():
   """
   Starting point of the application. This function manages the state of the application throughout its lifecycle.
   """
-  global State
+  State = 0
   while State != -1:
     clear()
     if State == 0:
-      main_page()
+      State = main_page()
     elif State == 1:
-      sign_up_page()
+      State = sign_up_page()
     elif State == 2:
-      login_page()
+      State = login_page()
     elif State == 3:
-      home_page()
+      State = home_page()
 
 
 if __name__ == "__main__":
