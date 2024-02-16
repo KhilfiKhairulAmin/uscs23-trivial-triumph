3067fb56ed194ec17c2a1ef8a372dc7e2c2a50c4
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-02-16 11:20:41 +0800

    Refactored codes

--- components/menus.py
+++ components/menus.py
@@ -28,22 +28,14 @@ def main_menu():
 
   # Prompt message
   center("Do you want to register or login?\n")
-  center("[1] Register    [2] Login    [3] Quit\n")
+  center("[1] Register    [2] Login\n")
 
-  while True:
-    try:
-      choice = int(prompt_choice(">", choices=[1, 2]))
-      
-      if choice == 1:
-        return 1  # Go to sign up
-      elif choice == 2:
-        return 2  # Go to login
-
-    except ValueError as err:
-      error(err)
-    except KeyboardInterrupt:
-      # If user press Ctrl+C, prompt to enter 3 for quitting the app
-      exit_modal()
+  choice = int(prompt_choice(">", choices=[1, 2]))
+  
+  if choice == 1:
+    return 1  # Go to sign up
+  elif choice == 2:
+    return 2  # Go to login
 
 
 def sign_up_menu():
@@ -56,23 +48,21 @@ def sign_up_menu():
 
   global Users, CurUser
   while True:
-    try:
-      username = prompt("Username: ")
-      password = prompt("Password: ", hidden=True)
-      repeat_password = prompt("Repeat password: ", hidden=True, input_width=24)
-      auth.sign_up(username, password, repeat_password, Users)  # Validate the input data for sign up
-      # Add the new user
-      Users[username] = [password, -1]
-      CurUser = username
-      # Save the updated data
-      db.save_users_data(Users)
-    # Go to home menu
-      return 3
-    except ValueError as err:
-      error(err)
-    # If user press Ctrl+C, go back to main menu
-    except KeyboardInterrupt:
-      return 0  # Go back to main menu
+    username = prompt("Username: ")
+    password = prompt("Password: ", hidden=True)
+    repeat_password = prompt("Repeat password: ", hidden=True, input_width=24)
+
+    if not auth.sign_up(username, password, repeat_password, Users):  # Validate the input data for sign up
+      continue
+
+    # Add the new user
+    Users[username] = [password, -1]
+    CurUser = username
+
+    # Save the updated data
+    db.save_users_data(Users)
+  # Go to home menu
+    return 3
 
 
 def login_menu():
@@ -84,20 +74,15 @@ def login_menu():
   center("Log In\n")
 
   while True:
-    try:
-      username = prompt("Username: ")
-      password = prompt("Password: ", hidden=True)
-      auth.log_in(username, password, Users)  # Validate the input data for sign up
-      # Update the global variables
-      global CurUser
-      CurUser = username
-      # Go to home menu
-      return 3
-    except ValueError as err:
-      error(err)
-    except KeyboardInterrupt:
-      # If user press Ctrl+C, go back to main menu
-      return 0
+    username = prompt("Username: ")
+    password = prompt("Password: ", hidden=True)
+    if not auth.log_in(username, password, Users):  # Validate the input data for sign up
+      continue
+    
+    # Update the global variables
+    global CurUser
+    CurUser = username
+    return 3  # Go to home menu
 
 
 def home_menu():
@@ -108,31 +93,25 @@ def home_menu():
   global CurUser
   display_header(subtitle=f"Welcome {CurUser}!")
 
-  while True:
-    try:
-      # Prompt message
-      center("Home Menu\n")
-      center("(1) Play       ")
-      center("(2) Leaderboard")
-      center("(3) Help       ")
-      center("(4) Logout     ")
-      center()
-      choice = int(prompt_choice(">", choices=[1, 2, 3, 4]))
+  # Prompt message
+  center("Home Menu\n")
+  center("(1) Play       ")
+  center("(2) Leaderboard")
+  center("(3) Help       ")
+  center("(4) Logout     ")
+  center()
 
-      if choice == 1:
-        return 4  # Go to quiz menu
-      elif choice == 2:
-        return 5  # Go to leaderboard menu
-      elif choice == 3:
-        return 6  # Go to help menu
-      elif choice == 4:
-        CurUser = ""  # Reset current user due to logout
-        return 0  # Back to main menu
-    except ValueError as err:
-      error(err)
-    except KeyboardInterrupt:
-      exit_modal()
-    
+  choice = int(prompt_choice(">", choices=[1, 2, 3, 4]))
+  if choice == 1:
+    return 4  # Go to quiz menu
+  elif choice == 2:
+    return 5  # Go to leaderboard menu
+  elif choice == 3:
+    return 6  # Go to help menu
+  elif choice == 4:
+    CurUser = ""  # Reset current user due to logout
+    return 0  # Back to main menu
+  
   
 def quiz_menu():
   clear()
@@ -199,17 +178,8 @@ def exit_modal(message="Are you sure you want to quit?"):
   center("[1] No    [2] Yes")
   center()
 
-  while True:
-    try:
-      choice = int(prompt_choice(">", choices=[1, 2]))
-      
-      if choice == 1:
-        return
-      elif choice == 2:
-        exit()
-
-    except ValueError as err:
-      error(err)
-    except KeyboardInterrupt:
-      # If user press Ctrl+C, prompt to enter 3 for quitting the app
-      return
+  choice = int(prompt_choice(">", choices=[1, 2]))
+  if choice == 1:
+    return
+  elif choice == 2:
+    exit()
