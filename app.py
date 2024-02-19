67ef0d2c005c5005214806949105f9cd85d2fcfd
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-02-19 15:56:29 +0800

    Update TODO

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-# TODO 15/5/2024
-# TODO Make leaderboard tracker (YASMIN)
-
-# TODO 16/4/2024
-# TODO Integrate Leaderboard menu
-
-# TODO Sometime later...
 # TODO Small Report about quiz app
-# TODO Update README file
 
 from menus import STATISTIC, exit_menu, help_menu, main_menu, player_statistics_menu, sign_up_menu, login_menu, home_menu, quiz_menu, leaderboard_menu, \
                              MAIN_MENU, SIGN_UP, LOGIN, HOME_MENU, QUIZ, LEADERBOARD, HELP, EXIT
