4f676677642d576e194970a2f85c1a16adf13441
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-02-19 15:43:05 +0800

    Add documentations and modified rules

--- auth.py
+++ auth.py
@@ -8,14 +8,21 @@ Provide interface for user authentications in Trivial Triumph
 from ui import error
 
 
-def sign_up(new_username: str, new_password: str, repeat_new_password: str, users: dict):
+MIN_CHAR_USERNAME = 3
+MAX_CHAR_USERNAME = 12
+
+MIN_CHAR_PASSWORD = 3
+MAX_CHAR_PASSWORD = 20
+
+
+def sign_up(new_username: str, new_password: str, repeat_new_password: str, users: dict) -> str:
   """
-  Register a new user
+  Register a new user. Returns username if authorized, else return empty string. This function automatically assigns the new user data into `users` variable if authorization successful.
   """
 
-  # Make sure username is at least 3 characters long and maximum 100 characters long
-  if len(new_username) < 3 or len(new_username) > 100:
-    error("Username must contain at least 3 characters and maximum of 100 characters")
+  # Make sure username is at least 3 characters long and maximum 12 characters long
+  if len(new_username) < MIN_CHAR_USERNAME or len(new_username) > MAX_CHAR_USERNAME:
+    error(f"Username must contain at least {MIN_CHAR_USERNAME} characters and maximum of {MAX_CHAR_USERNAME} characters")
     return ''
 
   # Make sure username is unique
@@ -31,9 +38,9 @@ def sign_up(new_username: str, new_password: str, repeat_new_password: str, user
     error("Username must consists of only alphanumeric characters")
     return ''
 
-  # Make sure password is at least 3 characters long and maximum 100 characters long
-  if len(new_password) < 3 or len(new_password) > 100:
-    error("Password must contain at least 3 characters and maximum of 100 characters")
+  # Make sure password is at least 3 characters long and maximum 20 characters long
+  if len(new_password) < MIN_CHAR_PASSWORD or len(new_password) > MAX_CHAR_PASSWORD:
+    error(f"Password must contain at least {MIN_CHAR_PASSWORD} characters and maximum of {MAX_CHAR_PASSWORD} characters")
     return ''
 
   if repeat_new_password != new_password:
@@ -46,14 +53,16 @@ def sign_up(new_username: str, new_password: str, repeat_new_password: str, user
   return new_username
 
 
-def log_in(username: str, password: str, users: dict):
+def log_in(username: str, password: str, users: dict) -> str:
   """
-  Authenticate existing user
+  Authenticate existing user. Returns username if authorized, else return empty string.
   """
+  # Check username existence
   if username not in users.keys():
     error("Username does not exist")
     return ''
 
+  # Verify password
   if users[username][0] != password:
     error("Username or password is false")
     return ''
@@ -68,4 +77,5 @@ if __name__ == "__main__":
     "yasmin": ["yasmin", 67, 100]
   }
   sign_up("hello", "hello", "hello", users)
+  print(users)
   log_in("hello", "hello", users)
